a903c25432e74a041bc0ee253d397e974c934f92
franco <franbayu_96@hotmail.com>
2022-01-17 12:18:35 -0300

    comienzo de proyecto

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 
-const {leerInput} = require('./helpers/inquirer')
+const {inquirerMenu, pausa, leerInput} = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async () =>{
-    const texto = await leerInput('Hola:');
-    console.log(texto);
+    let opt = 0;
+    const busquedas = new Busquedas();
+
+
+
+    do{
+        opt = await inquirerMenu();
+
+        switch(opt){
+            case 1:     
+            const lugar = await leerInput('Ciudad:');
+
+            console.log(lugar);
+                
+            console.log('\nInformación de la ciudad\n'.green)
+            console.log('Ciudad: ');
+            console.log('Latitud: ');
+            console.log('Longitud: ');
+            console.log('Temperatura actual: ');
+            console.log('Temperatura mínima: ');
+            console.log('Temperatura máxima: ');
+
+                break;
+            case 2:
+                break;
+        }
+        
+        if(opt!==0) await pausa();
+ 
+        
+    }while(opt !== 0)
+
+    console.log({opt});
+
+
 }
 
 main();
---
+++ models/busquedas.js
@@ -0,0 +1,17 @@
+class Busquedas{
+    historial = [];
+
+    constructor (){
+        // TODO: leer DB
+    }
+
+
+    async ciudad(lugar = ''){
+        //peticion http
+        console.log(lugar);
+
+        return []; //ciudades
+    }
+}
+
+module.exports = Busquedas;
